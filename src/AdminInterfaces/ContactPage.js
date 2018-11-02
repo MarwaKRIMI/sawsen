e05892d833088dad0821b4b04190877e041986b6
MarwaKRIMI <33938226+MarwaKRIMI@users.noreply.github.com>
2018-11-02 20:07:09 +0100

    Update ContactPage.js

--- src/AdminInterfaces/ContactPage.js
+++ src/AdminInterfaces/ContactPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Container, Row, Col, Card, CardBody, Fa, Button, Input } from 'mdbreact';
 import { withGoogleMap, GoogleMap } from 'react-google-maps';
 import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+
 import 'mdbreact/dist/css/mdb.css';
 import './contactPage.css'
 
